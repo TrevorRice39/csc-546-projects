83375ec74e44cc90952a944a7545e0e1fb8cca97
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2019-11-14 09:05:45 -0500

    using nn to predict heart disease

--- Project2/src/main.py
+++ Project2/src/main.py
@@ -2,153 +2,110 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import numpy as np
-import random
-from os import listdir
-from os.path import isfile, join
-from scipy import misc
-from matplotlib.image import imread
-from matplotlib import pyplot as plt
-path = os.getcwd()
-parasitized_path = path[0 : path.rfind('/')] + '/data/Parasitized'
-parasitized_images = [f for f in listdir(parasitized_path) if isfile(join(parasitized_path, f))]
-uninfected_path = path[0 : path.rfind('/')] + '/data/Uninfected'
-uninfected_images = [f for f in listdir(uninfected_path) if isfile(join(uninfected_path, f))]
 
+tf.set_random_seed(777)
 
-parasitized_path += '/'
-uninfected_path += '/'
-resized_p_images = []
-resized_u_images = []
-len_p = len(parasitized_images)
-len_u = len(uninfected_images)
-image_size = 128
-len_p = 1000
-len_u = 1000
-for i in range(len_p):
-    img = imread(parasitized_path + parasitized_images[i])
-    img = np.resize(img, (128, 128))
-    resized_p_images.append([img, 1])
-    if i%300 == 0:
-        print((i/len_p)* 100)
-for i in range(len_u):
-    img = imread(uninfected_path + uninfected_images[i])
-    img = np.resize(img, (128, 128))
-    resized_u_images.append([img, 0])
-    if i%300 == 0:
-        print((i/len_u)* 100)
+# function used for normalizing our data using min max
+def min_max_scaler(data):
+    numerator = data-np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+    return numerator/denominator+1e-7
 
+# creating the data path
+data_path = os.getcwd()
+data_path = data_path[0: data_path.rfind('/')] + '/data/heart.csv'
 
-learning_rate = 0.001
-training_epochs = 3
-batch_size = 100
+# loading the heart data and removing the titles
+heart_data = np.loadtxt(data_path, delimiter=',', dtype=np.float32, skiprows=1)
+normalize_list = [0, 3, 4, 7, 9] # columns to normalize
+for num in normalize_list:
+    heart_data[:, num] = min_max_scaler(heart_data[:, num]) # normalization using min max scaler
+np.random.shuffle(heart_data)
+print(heart_data)
+heart_data_train = heart_data[0 : int(len(heart_data) * 0.7)]
+heart_data_test = heart_data[int(len(heart_data) * 0.7) : ]
+print('__________________', len(heart_data_train))
+learning_rate = 0.01
+training_epochs = 1000
+batch_size = 200
+total_batch = len(heart_data)/batch_size
+print(total_batch)
+
+X = tf.placeholder(tf.float32, [None, 13])
+Y = tf.placeholder(tf.float32, [None, 2])
 
 keep_prob = tf.placeholder(tf.float32)
 
-class Model:
-	def __init__(self, sess, name):
-		self.sess = sess
-		self.name = name
-		self.build_net()
-	def build_net(self):
-		self.training = tf.placeholder(tf.bool)
-		self.X = tf.placeholder(tf.float32, [None ,image_size, image_size])
+W1 = tf.get_variable("W1", shape=[13, 7], initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([7]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-		X_img = tf.reshape(self.X, [-1, image_size, image_size, 1])
-		self.Y = tf.placeholder(tf.float32, [None, 2])
+W2 = tf.get_variable("W2", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([7]))
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-		conv1 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
-		pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], padding='SAME', strides=2)
-		dropout1 = tf.layers.dropout(inputs=pool1, rate=0.7, training=self.training)
+W3 = tf.get_variable("W3", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([7]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 
-		conv2 = tf.layers.conv2d(inputs=dropout1, filters=32, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
-		pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], padding='SAME', strides=2)
-		dropout2 = tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
+W4 = tf.get_variable("W4", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([7]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
-		conv3 = tf.layers.conv2d(inputs=dropout2, filters=32, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
-		pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], padding='SAME', strides=2)
-		dropout3= tf.layers.dropout(inputs=pool3, rate=0.7, training=self.training)
+# W5 = tf.get_variable("W5", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
+# b5 = tf.Variable(tf.random_normal([7]))
+# L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)
+# L5 = tf.nn.dropout(L5, keep_prob=keep_prob) 
 
-		flat = tf.reshape(dropout3, [-1, 128 * 4 * 4 * 4])
-		dense4 = tf.layers.dense(inputs=flat, units=500, activation=tf.nn.relu)
-		dropout4 = tf.layers.dropout(inputs=dense4, rate=0.5, training=self.training)
-		self.logits = tf.layers.dense(inputs=dropout4, units=2)
+W6 = tf.get_variable("W6", shape=[7, 2], initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.random_normal([2]))
+hypothesis = tf.matmul(L4, W6) + b6
 
-		self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = self.logits, labels = self.Y))
-		self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
-		self.prediction = tf.argmax(self.logits, 1)
-		self.correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
-		self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-
-	def predict(self, x_test, training=False):
-		return self.sess.run(self.prediction, feed_dict={self.X: x_test, self.Y: [[1, 1]], self.training: training})
-	def get_accuracy(self, x_test, y_test, training=False):
-		return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
-	def train(self, x_data,  y_data, training=True):
-		return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
-	
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 sess = tf.Session()
-m = Model(sess, "model1")
-data = resized_u_images + resized_p_images
-np.random.shuffle(data)
-train_data = data[0 : int(.7 * len(data))]
-test_data = data[int(.7 * len(data)) + 1 : ]
 sess.run(tf.global_variables_initializer())
-print(len(train_data))
-print(len(test_data))
 i = 0
 for epoch in range(training_epochs):
-	total_batch = int(len(train_data)/batch_size)
-	print(total_batch)
 	avg_cost = 0
-	
-	for i in range(total_batch):
-		x_data = []
-		y_data = []
-		for x in range(batch_size):
-			elem = train_data[i]
-			i += 1
-			x_data.append(elem[0])
-			if elem[1] == 0:
-				y_data.append([1, 0])
+
+	for j in range(int(total_batch)):
+		data = heart_data_train[100 * j : 100 * (j+1)]
+		#print(data)
+		dataX = data[: , 0 : -1]
+		dataY = (data[:, [-1]])
+		one_hot_y = []
+		for y in dataY:
+			if y == 1:
+				one_hot_y.append([0, 1])
 			else:
-				y_data.append([0, 1])
-		c, _ = m.train(x_data, y_data)
-		avg_cost += c / total_batch
-	print("Average cost = ", avg_cost, " for Epoch " , + epoch)
+				one_hot_y.append([1, 0])
+		feed_dict = {X: dataX, Y: one_hot_y, keep_prob: 0.7}
+		c, _ , h= sess.run([cost, optimizer, hypothesis], feed_dict=feed_dict)
+		avg_cost += c/total_batch
+	print('Epoch:', '%04d' % (epoch + 1), 'cost=','{:.9f}'.format(avg_cost))
 
-x_data = []
-y_data = []
-for x in test_data:
-	x_data.append(x[0])
-	if x[1] == 0:
-		y_data.append([1, 0])
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+dataX = heart_data_test[:, 0 : -1]
+dataY = heart_data_test[:, [-1]]
+one_hot_y = []
+for y in dataY:
+	if y == 1:
+		one_hot_y.append([0, 1])
 	else:
-		y_data.append([0, 1])
-print("Accuracy: ", m.get_accuracy(x_data, y_data))
+		one_hot_y.append([1, 0])
+print('Accuracy:', sess.run(accuracy, feed_dict = {X:dataX, Y: one_hot_y, keep_prob: 1 }))
 
-# x_data = np.array(x_data)
-# y_data = np.array(y_data)
-# print(x_data.shape)
-# print(y_data)
+np.random.shuffle(heart_data)
+for i in range(10):
+	test = heart_data[i : i+1, :]
+	xtest = test[:, 0 : -1]
 
-# m.train(x_data, y_data)
-
-# print('Accuracy', m.get_accuracy(x_data, y_data))
-
-# cont = "y"
-# i = 0
-# while cont == "y":
-	
-# 	data = resized_u_images[i]
-# 	plt.imshow(data[0])
-# 	plt.show()
-# 	p = m.predict([data[0]])
-# 	print(p, 0)
-
-# 	data = resized_p_images[i]
-# 	p = m.predict([data[0]])
-# 	print(p, 1)
-# 	i += 1
-	
-# 	cont = input("Continue? y or n")
+	print("label", test[:, [-1]], end = '')
+	print(" prediction", sess.run(tf.argmax(hypothesis, 1), feed_dict={X: xtest, keep_prob: 1}))
