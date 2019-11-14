a0d714d2b6505fd7c84d63ddeafdfabf1cf39533
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2019-11-14 10:49:10 -0500

    modified learning rate

--- Project2/src/main.py
+++ Project2/src/main.py
@@ -25,8 +25,8 @@ print(heart_data)
 heart_data_train = heart_data[0 : int(len(heart_data) * 0.7)]
 heart_data_test = heart_data[int(len(heart_data) * 0.7) : ]
 print('__________________', len(heart_data_train))
-learning_rate = 0.01
-training_epochs = 1000
+learning_rate = 0.001
+training_epochs = 10
 batch_size = 200
 total_batch = len(heart_data)/batch_size
 print(total_batch)
@@ -35,35 +35,47 @@ X = tf.placeholder(tf.float32, [None, 13])
 Y = tf.placeholder(tf.float32, [None, 2])
 
 keep_prob = tf.placeholder(tf.float32)
-
-W1 = tf.get_variable("W1", shape=[13, 7], initializer=tf.contrib.layers.xavier_initializer())
-b1 = tf.Variable(tf.random_normal([7]))
+depth = 100
+W1 = tf.get_variable("W1", shape=[13, depth], initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([depth]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
-L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
+#L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-W2 = tf.get_variable("W2", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
-b2 = tf.Variable(tf.random_normal([7]))
+W2 = tf.get_variable("W2", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([depth]))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
-L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+#L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.get_variable("W3", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
-b3 = tf.Variable(tf.random_normal([7]))
+W3 = tf.get_variable("W3", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([depth]))
 L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
-L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+#L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 
-W4 = tf.get_variable("W4", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
-b4 = tf.Variable(tf.random_normal([7]))
+W4 = tf.get_variable("W4", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([depth]))
 L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
 L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
-# W5 = tf.get_variable("W5", shape=[7, 7], initializer=tf.contrib.layers.xavier_initializer())
-# b5 = tf.Variable(tf.random_normal([7]))
-# L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)
+W5 = tf.get_variable("W5", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([depth]))
+L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)
 # L5 = tf.nn.dropout(L5, keep_prob=keep_prob) 
 
-W6 = tf.get_variable("W6", shape=[7, 2], initializer=tf.contrib.layers.xavier_initializer())
-b6 = tf.Variable(tf.random_normal([2]))
-hypothesis = tf.matmul(L4, W6) + b6
+W6 = tf.get_variable("W6", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.random_normal([depth]))
+L6 = tf.nn.relu(tf.matmul(L5, W6) + b6)
+
+W7 = tf.get_variable("W7", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.random_normal([depth]))
+L7 = tf.nn.relu(tf.matmul(L6, W7) + b7)
+
+W8 = tf.get_variable("W8", shape=[depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+b8 = tf.Variable(tf.random_normal([depth]))
+L8 = tf.nn.relu(tf.matmul(L7, W8) + b8)
+
+W9 = tf.get_variable("W9", shape=[depth, 2], initializer=tf.contrib.layers.xavier_initializer())
+b9 = tf.Variable(tf.random_normal([2]))
+hypothesis = tf.matmul(L8, W9) + b9
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
